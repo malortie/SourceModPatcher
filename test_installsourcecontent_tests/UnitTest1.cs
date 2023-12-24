c21a1613fa8958905da5d4323ca55c7960c1337a
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2023-12-24 16:33:38 -0500

    Update UnitTest1.cs

--- test_installsourcecontent_tests/UnitTest1.cs
+++ test_installsourcecontent_tests/UnitTest1.cs
@@ -3,12 +3,39 @@ using test_installsourcecontent;
 
 namespace test_installsourcecontent_tests
 {
+    public class NullWriter : IWriter
+    {
+        public void Success(string message) {}
+        public void Info(string message) {}
+        public void Warning(string message) {}
+        public void Error(string message) {}
+        public void Failure(string message) {}
+        public void Cancellation(string message) {}
+    }
+
+    public class AllowAllVPKFiles : IVPKFileFilter
+    {
+        public bool PassesFilter(string vpkFile) => true;
+    }
+
+    public class ExcludeAllVPKFiles : IVPKFileFilter
+    {
+        public bool PassesFilter(string vpkFile) => false;
+    }
+
+    public class NoSoundWavFilter : IVPKFileFilter
+    {
+        public bool PassesFilter(string vpkFile)
+        {
+            return vpkFile != "sound.wav";
+        }
+    }
+
     [TestClass]
     public class TestVPKExtractor
     {
-
         [TestMethod]
-        public void TestVPKExtractorTest()
+        public void NoFilesExtractedWithExcludeAllFilter()
         {
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
             {
@@ -16,11 +43,44 @@ namespace test_installsourcecontent_tests
             });
 
             var extractor = new VPKExtractor();
-            extractor.Extract(fileSystem, new NullWriter(), "C:/vpks/simple_vpk.vpk", "C:/output");
+            extractor.Extract(fileSystem, new NullWriter(), "C:/vpks/simple_vpk.vpk", "C:/output", new ExcludeAllVPKFiles());
+            var expected = new string[] {};
+            var actual = fileSystem.Directory.GetFiles("C:/output").Select(a => PathExtensions.ConvertToUnixDirectorySeparator(fileSystem, a)).ToArray();
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void AllFilesExtractedWithAllowAllFilter()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "C:/vpks/simple_vpk.vpk", new MockFileData(File.ReadAllBytes("../../../data/vpks/simple_vpk.vpk")) }
+            });
+
+            var extractor = new VPKExtractor();
+            extractor.Extract(fileSystem, new NullWriter(), "C:/vpks/simple_vpk.vpk", "C:/output", new AllowAllVPKFiles());
             var expected = new string[] {
                 "C:/output/file1.txt",
                 "C:/output/model.mdl",
                 "C:/output/sound.wav",
+            };
+            var actual = fileSystem.Directory.GetFiles("C:/output").Select(a => PathExtensions.ConvertToUnixDirectorySeparator(fileSystem, a)).ToArray();
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void SpecificFilesExtractedWithCustomFilter()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "C:/vpks/simple_vpk.vpk", new MockFileData(File.ReadAllBytes("../../../data/vpks/simple_vpk.vpk")) }
+            });
+
+            var extractor = new VPKExtractor();
+            extractor.Extract(fileSystem, new NullWriter(), "C:/vpks/simple_vpk.vpk", "C:/output", new NoSoundWavFilter());
+            var expected = new string[] {
+                "C:/output/file1.txt",
+                "C:/output/model.mdl"
             };
             var actual = fileSystem.Directory.GetFiles("C:/output").Select(a => PathExtensions.ConvertToUnixDirectorySeparator(fileSystem, a)).ToArray();
             CollectionAssert.AreEquivalent(expected, actual);
