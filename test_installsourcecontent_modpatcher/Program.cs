0c029ed441380421b1739578ba09183be2d3ff89
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2023-12-30 08:20:03 -0500

    Commit modpatcher draft.

---
+++ test_installsourcecontent_modpatcher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace test_installsourcecontent_modpatcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
