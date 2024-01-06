6fcfa015d3e6b0069c1064793e2d7d3b356c21e8
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-06 10:25:38 -0500

    Add FileTokenReplacer tests.

---
+++ test_installsourcecontent_modpatcher_tests/TestFileTokenReplacer.cs
@@ -0,0 +1,33 @@
+using System.Collections.ObjectModel;
+using Pipelines;
+using System.IO.Abstractions.TestingHelpers;
+using test_installsourcecontent_modpatcher;
+
+namespace test_installsourcecontent_modpatcher_tests
+{
+    [TestClass]
+    public class TestFileTokenReplacer
+    {
+        [TestMethod]
+        public void ReplaceInFile_Simple()
+        {
+            const string FILE_PATH = "C:/source/test1.txt";
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { FILE_PATH, new MockFileData("${{token1}} ${{token2}}") },
+            });
+
+            var fileTokenCopier = new FileTokenReplacer(new TokenReplacer {
+                Prefix = "${{",
+                Suffix = "}}"
+            });
+            fileTokenCopier.Variables = new ReadOnlyDictionary<string, string>(new Dictionary<string, string>(){
+                {"token1", "hello"},
+                {"token2", "world"}
+            });
+
+            Assert.IsTrue(fileTokenCopier.ReplaceInFile(fileSystem, new NullWriter(), FILE_PATH));
+            Assert.AreEqual("hello world", fileSystem.File.ReadAllText(FILE_PATH));
+        }
+    }
+}
