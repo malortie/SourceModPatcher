9c6d3ef146e5057573fa6b8634ad4488e49b2308
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-07 13:07:24 -0500

    Add PipelineStageProgressStepFormatter tests.

---
+++ Pipelines.Tests/TestPipelineStage_PipelineStageProgressStepFormatter.cs
@@ -0,0 +1,436 @@
+namespace Pipelines.Tests
+{
+    public class FormatterMock : IPipelineStageProgressStepFormatter
+    {
+        public int FormatTotal { get; private set; } = 0;
+
+        public string Format(IPipelineProgressContext pipelineContext, ReadOnlyPipelineStepData stepData)
+        {
+            ++FormatTotal;
+            return string.Empty;
+        }
+    }
+
+    [TestClass]
+    public class TestPipelineStage_PipelineStageProgressStepFormatter
+    {
+        static NullContext NullContext = new NullContext();
+        static IWriter NullWriter = new NullWriter();
+
+        [TestMethod]
+        public void StepExecuteFormatter_Called_WhenAllDependenciesWereCompleted()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataComplete
+                    {
+                        Name = "step1"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step2"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step3",
+                        DependsOn = ["step1", "step2"]
+                    },
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(3, stepExecuteFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepExecuteFormatter_NotCalledOnDependents_WhenAtLeastOneDependencyWasNotCompleted()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataComplete
+                    {
+                        Name = "step1"
+                    },
+                    new NullStepDataPartiallyComplete
+                    {
+                        Name = "step2"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step3",
+                        DependsOn = ["step1", "step2"]
+                    },
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(2, stepExecuteFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepExecuteFormatter_NotCalledOnDependents_WhenAllDependenciesWereNotCompleted()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataPartiallyComplete
+                    {
+                        Name = "step1"
+                    },
+                    new NullStepDataPartiallyComplete
+                    {
+                        Name = "step2"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step3",
+                        DependsOn = ["step1", "step2"]
+                    },
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(2, stepExecuteFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepDependenciesNotCompletedFormatter_NotCalled_WhenAllDependenciesWereCompleted()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataComplete
+                    {
+                        Name = "step1"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step2"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step3",
+                        DependsOn = ["step1", "step2"]
+                    },
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(0, stepDependenciesNotCompletedFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepDependenciesNotCompletedFormatter_CalledOnDependents_WhenAtLeastOneDependencyWasNotCompleted()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataComplete
+                    {
+                        Name = "step1"
+                    },
+                    new NullStepDataPartiallyComplete
+                    {
+                        Name = "step2"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step3",
+                        DependsOn = ["step1", "step2"]
+                    },
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(1, stepDependenciesNotCompletedFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepDependenciesNotCompletedFormatter_CalledOnDependents_WhenAllDependenciesWereNotCompleted()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataPartiallyComplete
+                    {
+                        Name = "step1"
+                    },
+                    new NullStepDataPartiallyComplete
+                    {
+                        Name = "step2"
+                    },
+                    new NullStepDataComplete
+                    {
+                        Name = "step3",
+                        DependsOn = ["step1", "step2"]
+                    },
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(1, stepDependenciesNotCompletedFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepCompletedFormatter_Called_3_Times()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataComplete(),
+                    new NullStepDataComplete(),
+                    new NullStepDataComplete(),
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(3, stepCompletedFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepPartiallyCompletedFormatter_Called_3_Times()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataPartiallyComplete(),
+                    new NullStepDataPartiallyComplete(),
+                    new NullStepDataPartiallyComplete(),
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(3, stepPartiallyCompletedFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepFailedFormatter_Called_3_Times()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataFailed(),
+                    new NullStepDataFailed(),
+                    new NullStepDataFailed(),
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(3, stepFailedFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepCancelledFormatter_Called_3_Times()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataCancelled(),
+                    new NullStepDataCancelled(),
+                    new NullStepDataCancelled(),
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(3, stepCancelledFormatter.FormatTotal);
+        }
+
+        [TestMethod]
+        public void StepFormatter_OneOfEachType()
+        {
+            var stepExecuteFormatter = new FormatterMock();
+            var stepDependenciesNotCompletedFormatter = new FormatterMock();
+            var stepCompletedFormatter = new FormatterMock();
+            var stepPartiallyCompletedFormatter = new FormatterMock();
+            var stepFailedFormatter = new FormatterMock();
+            var stepCancelledFormatter = new FormatterMock();
+
+            var stage = new NullStage
+            {
+                ProgressWriter = new PipelineStageProgressWriter(NullWriter)
+                {
+                    StepExecuteFormatter = stepExecuteFormatter,
+                    StepDependenciesNotCompletedFormatter = stepDependenciesNotCompletedFormatter,
+                    StepCompletedFormatter = stepCompletedFormatter,
+                    StepPartiallyCompletedFormatter = stepPartiallyCompletedFormatter,
+                    StepFailedFormatter = stepFailedFormatter,
+                    StepCancelledFormatter = stepCancelledFormatter,
+                },
+                StepsDatas = [
+                    new NullStepDataComplete(),
+                    new NullStepDataPartiallyComplete(),
+                    new NullStepDataFailed(),
+                    new NullStepDataCancelled(),
+                ]
+            };
+
+            stage.DoStage(NullContext);
+            Assert.AreEqual(1, stepCompletedFormatter.FormatTotal);
+            Assert.AreEqual(1, stepPartiallyCompletedFormatter.FormatTotal);
+            Assert.AreEqual(1, stepFailedFormatter.FormatTotal);
+            Assert.AreEqual(1, stepCancelledFormatter.FormatTotal);
+        }
+    }
+}
