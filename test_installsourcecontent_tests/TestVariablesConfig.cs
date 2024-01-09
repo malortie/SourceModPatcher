8dbca661aa11097a93c649c81854dc32e14eca69
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-09 18:54:55 -0500

    Add VariablesConfig tests.

--- test_installsourcecontent_tests/TestVariablesConfig.cs
+++ test_installsourcecontent_tests/TestVariablesConfig.cs
@@ -1,3 +1,5 @@
+using Pipelines;
+using System.IO.Abstractions.TestingHelpers;
 using test_installsourcecontent;
 
 namespace test_installsourcecontent_tests
@@ -5,6 +7,8 @@ namespace test_installsourcecontent_tests
     [TestClass]
     public class TestVariablesConfig
     {
+        static IWriter NullWriter = new NullWriter();
+
         [TestMethod]
         public void Serialize_JSONVariablesConfig()
         {
@@ -39,6 +43,60 @@ namespace test_installsourcecontent_tests
             Assert.AreEqual("C:/Documents/SourceContent/hl2", deserialized["hl2_content_path"]);
             Assert.AreEqual("C:/Documents/SourceContent/sdkbase2006", deserialized["sdkbase2006_content_path"]);
             Assert.AreEqual("C:/Documents/SourceContent/sdkbase2007", deserialized["sdkbase2007_content_path"]);
+        }
+
+        [TestMethod]
+        public void LoadConfig_Simple()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/variables.json", new MockFileData(File.ReadAllBytes("../../../data/config/variables.json")) }
+            });
+
+            var variablesConfig = new VariablesConfig(fileSystem, NullWriter, "C:/variables.json", new JSONConfigurationSerializer<JSONVariablesConfig>());
+
+            variablesConfig.LoadConfig();
+
+            var config = variablesConfig.Config;
+            Assert.IsNotNull(config);
+            Assert.IsTrue(config.ContainsKey("hl2_content_path"));
+            Assert.IsTrue(config.ContainsKey("sdkbase2006_content_path"));
+            Assert.IsTrue(config.ContainsKey("sdkbase2007_content_path"));
+            Assert.AreEqual("C:/Half-Life 2", config["hl2_content_path"]);
+            Assert.AreEqual("C:/Source SDK Base", config["sdkbase2006_content_path"]);
+            Assert.AreEqual("C:/Source SDK Base 2007", config["sdkbase2007_content_path"]);
+        }
+
+        [TestMethod]
+        public void SaveConfig_EmptyWhenNoVariables()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> { });
+
+            var variablesConfig = new VariablesConfig(fileSystem, NullWriter, "C:/variables.json", new JSONConfigurationSerializer<JSONVariablesConfig>());
+
+            variablesConfig.SaveConfig();
+
+            Assert.AreEqual("{}", fileSystem.File.ReadAllText("C:/variables.json"));
+        }
+
+        [TestMethod]
+        public void SaveVariable_Simple()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> { });
+
+            var variablesConfig = new VariablesConfig(fileSystem, NullWriter, "C:/variables.json", new JSONConfigurationSerializer<JSONVariablesConfig>());
+
+            variablesConfig.SaveVariable("hl2_content_path", "C:/Half-Life 2");
+            variablesConfig.SaveVariable("sdkbase2006_content_path", "C:/Source SDK Base");
+            variablesConfig.SaveVariable("sdkbase2007_content_path", "C:/Source SDK Base 2007");
+
+            const string expected = """
+                {
+                  "hl2_content_path": "C:/Half-Life 2",
+                  "sdkbase2006_content_path": "C:/Source SDK Base",
+                  "sdkbase2007_content_path": "C:/Source SDK Base 2007"
+                }
+                """;
+            Assert.AreEqual(expected, fileSystem.File.ReadAllText("C:/variables.json"));
         }
     }
 }
