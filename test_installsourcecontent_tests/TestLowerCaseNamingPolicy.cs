f8605a56245f66443b0c8b37e5f42cbd0f16a3a6
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-09 18:17:54 -0500

    Add LowerCaseNamingPolicy tests.

---
+++ test_installsourcecontent_tests/TestLowerCaseNamingPolicy.cs
@@ -0,0 +1,25 @@
+using System.Text;
+using test_installsourcecontent;
+
+namespace test_installsourcecontent_tests
+{
+    [TestClass]
+    public class TestLowerCaseNamingPolicy
+    {
+        [TestMethod]
+        public void ConvertName_ShouldBeLowerCase_WhenInputIsUpperCase()
+        {
+            const string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var namingPolicy = new LowerCaseNamingPolicy();
+            Assert.AreEqual(input.ToLower(), namingPolicy.ConvertName(input));
+        }
+
+        [TestMethod]
+        public void ConvertName_ShouldBeLowerCase_WhenInputIsLowerCase()
+        {
+            const string input = "abcdefghijklmnopqrstuvwxyz";
+            var namingPolicy = new LowerCaseNamingPolicy();
+            Assert.AreEqual(input, namingPolicy.ConvertName(input));
+        }
+    }
+}
