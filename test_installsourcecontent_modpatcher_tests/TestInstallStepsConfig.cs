e342e46cda456d57a5183b9e926f4667f04677a0
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-09 19:55:30 -0500

    Add InstallStepsConfig tests.

--- test_installsourcecontent_modpatcher_tests/TestInstallStepsConfig.cs
+++ test_installsourcecontent_modpatcher_tests/TestInstallStepsConfig.cs
@@ -1,11 +1,16 @@
 using test_installsourcecontent;
 using JSONInstallStepsConfig = test_installsourcecontent_modpatcher.JSONInstallStepsConfig;
+using InstallStepsConfig = test_installsourcecontent_modpatcher.InstallStepsConfig;
+using Pipelines;
+using System.IO.Abstractions.TestingHelpers;
 
 namespace test_installsourcecontent_modpatcher_tests
 {
     [TestClass]
     public class TestInstallStepsConfig
     {
+        static IWriter NullWriter = new NullWriter();
+
         [TestMethod]
         public void Deserialize_JSONInstallStepsConfig()
         {
@@ -26,6 +31,27 @@ namespace test_installsourcecontent_modpatcher_tests
             Assert.AreEqual("./sourcemod_1.install.steps.json", deserialized["sourcemod_1"]);
             Assert.AreEqual("./sourcemod_2.install.steps.json", deserialized["sourcemod_2"]);
             Assert.AreEqual("./sourcemod_3.install.steps.json", deserialized["sourcemod_3"]);
+        }
+
+        [TestMethod]
+        public void LoadConfig_Simple()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.install.steps.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.install.steps.json")) }
+            });
+
+            var installStepsConfig = new InstallStepsConfig(fileSystem, NullWriter, "C:/sourcemods.install.steps.json", new JSONConfigurationSerializer<JSONInstallStepsConfig>());
+            installStepsConfig.LoadConfig();
+
+            var config = installStepsConfig.Config;
+            Assert.IsNotNull(config);
+            Assert.IsTrue(config.ContainsKey("sourcemod_1"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_2"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_3"));
+
+            Assert.AreEqual("C:/sourcemod_1.install.steps.json", config["sourcemod_1"]);
+            Assert.AreEqual("C:/sourcemod_2.install.steps.json", config["sourcemod_2"]);
+            Assert.AreEqual("C:/sourcemod_3.install.steps.json", config["sourcemod_3"]);
         }
     }
 }
