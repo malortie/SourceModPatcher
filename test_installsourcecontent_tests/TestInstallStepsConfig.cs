75fa283a9d88abd85ea4e28bce1c46bd25342059
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-09 19:16:27 -0500

    Add InstallStepsConfig tests.

--- test_installsourcecontent_tests/TestInstallStepsConfig.cs
+++ test_installsourcecontent_tests/TestInstallStepsConfig.cs
@@ -1,3 +1,5 @@
+using Pipelines;
+using System.IO.Abstractions.TestingHelpers;
 using test_installsourcecontent;
 
 namespace test_installsourcecontent_tests
@@ -5,6 +7,8 @@ namespace test_installsourcecontent_tests
     [TestClass]
     public class TestInstallStepsConfig
     {
+        static IWriter NullWriter = new NullWriter();
+
         [TestMethod]
         public void Deserialize_JSONInstallStepsConfig()
         {
@@ -25,6 +29,29 @@ namespace test_installsourcecontent_tests
             Assert.AreEqual("./215.install.steps.json", deserialized[215]);
             Assert.AreEqual("./218.install.steps.json", deserialized[218]);
             Assert.AreEqual("./220.install.steps.json", deserialized[220]);
+        }
+
+        [TestMethod]
+        public void LoadConfig_Simple()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/steamapps.install.steps.json", new MockFileData(File.ReadAllBytes("../../../data/config/steamapps.install.steps.json")) }
+            });
+
+            var installStepsConfig = new InstallStepsConfig(fileSystem, NullWriter, "C:/steamapps.install.steps.json", new JSONConfigurationSerializer<JSONInstallStepsConfig>());
+            installStepsConfig.LoadConfig();
+
+            var config = installStepsConfig.Config;
+            Assert.IsNotNull(config);
+            Assert.IsTrue(config.ContainsKey(215));
+            Assert.IsTrue(config.ContainsKey(218));
+            Assert.IsTrue(config.ContainsKey(220));
+            Assert.IsTrue(config.ContainsKey(240));
+
+            Assert.AreEqual("C:/215.install.steps.json", config[215]);
+            Assert.AreEqual("C:/218.install.steps.json", config[218]);
+            Assert.AreEqual("C:/220.install.steps.json", config[220]);
+            Assert.AreEqual("C:/240.install.steps.json", config[240]);
         }
     }
 }
