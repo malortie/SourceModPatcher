478940945f7b2a677a1f63165165c8fdff58c72e
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-09 19:48:32 -0500

    Add InstallSettings tests.

--- test_installsourcecontent_modpatcher_tests/TestInstallSettings.cs
+++ test_installsourcecontent_modpatcher_tests/TestInstallSettings.cs
@@ -1,12 +1,17 @@
 using test_installsourcecontent;
 using JSONInstallSettingsEntry = test_installsourcecontent_modpatcher.JSONInstallSettingsEntry;
 using JSONInstallSettings = test_installsourcecontent_modpatcher.JSONInstallSettings;
+using InstallSettings = test_installsourcecontent_modpatcher.InstallSettings;
+using System.IO.Abstractions.TestingHelpers;
+using Pipelines;
 
 namespace test_installsourcecontent_modpatcher_tests
 {
     [TestClass]
     public class TestInstallSettings
     {
+        static IWriter NullWriter = new NullWriter();
+
         [TestMethod]
         public void Deserialize_JSONInstallSettingsEntry()
         {
@@ -47,6 +52,58 @@ namespace test_installsourcecontent_modpatcher_tests
             Assert.IsFalse(deserialized["sourcemod_1"].Install);
             Assert.IsTrue(deserialized["sourcemod_2"].Install);
             Assert.IsFalse(deserialized["sourcemod_3"].Install);
+        }
+
+        [TestMethod]
+        public void LoadConfig_Simple()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.install.settings.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.install.settings.json")) }
+            });
+
+            var installSettings = new InstallSettings(fileSystem, NullWriter, "C:/sourcemods.install.settings.json", new JSONConfigurationSerializer<JSONInstallSettings>());
+            installSettings.LoadConfig();
+
+            var config = installSettings.Config;
+            Assert.IsNotNull(config);
+            Assert.IsTrue(config.ContainsKey("sourcemod_1"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_2"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_3"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_4"));
+
+            Assert.IsFalse(config["sourcemod_1"].Install);
+            Assert.IsTrue(config["sourcemod_2"].Install);
+            Assert.IsFalse(config["sourcemod_3"].Install);
+            Assert.IsTrue(config["sourcemod_4"].Install);
+        }
+
+        [TestMethod]
+        public void GetSourceModsToInstall()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.install.settings.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.install.settings.json")) }
+            });
+
+            var installSettings = new InstallSettings(fileSystem, NullWriter, "C:/sourcemods.install.settings.json", new JSONConfigurationSerializer<JSONInstallSettings>());
+            installSettings.LoadConfig();
+
+            CollectionAssert.AreEquivalent(new List<string> { "sourcemod_2", "sourcemod_4" }, installSettings.GetSourceModsToInstall());
+        }
+
+        [TestMethod]
+        public void IsSourceModMarkedForInstall()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.install.settings.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.install.settings.json")) }
+            });
+
+            var installSettings = new InstallSettings(fileSystem, NullWriter, "C:/sourcemods.install.settings.json", new JSONConfigurationSerializer<JSONInstallSettings>());
+            installSettings.LoadConfig();
+
+            Assert.IsFalse(installSettings.IsSourceModMarkedForInstall("sourcemod_1"));
+            Assert.IsTrue(installSettings.IsSourceModMarkedForInstall("sourcemod_2"));
+            Assert.IsFalse(installSettings.IsSourceModMarkedForInstall("sourcemod_3"));
+            Assert.IsTrue(installSettings.IsSourceModMarkedForInstall("sourcemod_4"));
         }
     }
 }
