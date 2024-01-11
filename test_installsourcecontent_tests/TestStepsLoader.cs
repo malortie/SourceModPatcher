db877a9075d9997aaa88fb9bde35d919d13e28dd
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-10 19:12:15 -0500

    Add StepsLoader tests.
    Add StepsLoader tests.

---
+++ test_installsourcecontent_tests/TestStepsLoader.cs
@@ -0,0 +1,30 @@
+using Pipelines;
+using System.IO.Abstractions.TestingHelpers;
+using test_installsourcecontent;
+
+namespace test_installsourcecontent_tests
+{
+    public class NullStepsMapper<ConfigT> : IStepMapper<ConfigT>
+    {
+        public IPipelineStepData Map(ConfigT jsonInstallStep)
+        {
+            return null;
+        }
+    }
+
+    [TestClass]
+    public class TestStepsLoader
+    {
+        [TestMethod]
+        public void Load_ThrowException_WhenDeserializedDataIsNull()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                 { "C:/steps.json", new MockFileData("") }
+            });
+
+            var steps = new StepsLoader<object>(fileSystem, new NullWriter(), new NullConfigurationSerializer<IList<object>>(), new NullStepsMapper<object>());
+
+            Assert.ThrowsException<Exception>(() => steps.Load("C:/steps.json"));
+        }
+    }
+}
