dee4b8634362473561ac547f7ad5308932b5186b
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-01-21 14:18:08 -0500

    Add more SourceModsConfig tests.

--- test_installsourcecontent_modpatcher_tests/TestSourceModsConfig.cs
+++ test_installsourcecontent_modpatcher_tests/TestSourceModsConfig.cs
@@ -1,3 +1,5 @@
+using Pipelines;
+using System.IO.Abstractions.TestingHelpers;
 using test_installsourcecontent;
 using test_installsourcecontent_modpatcher;
 
@@ -6,6 +8,8 @@ namespace test_installsourcecontent_modpatcher_tests
     [TestClass]
     public class TestSourceModsConfig
     {
+        static IWriter NullWriter = new NullWriter();
+
         [TestMethod]
         public void Deserialize_JSONSourceModsConfigEntry()
         {
@@ -70,6 +74,167 @@ namespace test_installsourcecontent_modpatcher_tests
             Assert.AreEqual("Source mod 3", sourceMod.Name);
             Assert.AreEqual("SourceMod_3", sourceMod.SourceModFolder);
             Assert.AreEqual("./data/mods/SourceMod_3", sourceMod.DataDir);
+        }
+
+        [TestMethod]
+        public void LoadConfig_Simple()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+            });
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), string.Empty);
+
+            sourceModsConfig.LoadConfig();
+
+            var config = sourceModsConfig.Config;
+            Assert.IsNotNull(config);
+            Assert.IsTrue(config.ContainsKey("sourcemod_1"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_2"));
+            Assert.IsTrue(config.ContainsKey("sourcemod_3"));
+
+            var sourceMod = config["sourcemod_1"];
+            Assert.AreEqual("Source mod 1", sourceMod.Name);
+            Assert.AreEqual("sourcemod_1_folder", sourceMod.SourceModFolder);
+            Assert.AreEqual("./data/mods/sourcemod_1", sourceMod.DataDir);
+
+            sourceMod = config["sourcemod_2"];
+            Assert.AreEqual("Source mod 2", sourceMod.Name);
+            Assert.AreEqual("sourcemod_2_folder", sourceMod.SourceModFolder);
+            Assert.AreEqual("./data/mods/sourcemod_2", sourceMod.DataDir);
+
+            sourceMod = config["sourcemod_3"];
+            Assert.AreEqual("Source mod 3", sourceMod.Name);
+            Assert.AreEqual("sourcemod_3_folder", sourceMod.SourceModFolder);
+            Assert.AreEqual("./data/mods/sourcemod_3", sourceMod.DataDir);
+        }
+
+        [TestMethod]
+        public void SourceModInstallPath_Property()
+        {
+            var fileSystem = new MockFileSystem();
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), "123");
+
+            Assert.AreEqual("123", sourceModsConfig.SourceModInstallPath);
+        }
+
+        [TestMethod]
+        public void GetSourceModName()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+            });
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), string.Empty);
+
+            sourceModsConfig.LoadConfig();
+
+            Assert.AreEqual("Source mod 1", sourceModsConfig.GetSourceModName("sourcemod_1"));
+            Assert.AreEqual("Source mod 2", sourceModsConfig.GetSourceModName("sourcemod_2"));
+            Assert.AreEqual("Source mod 3", sourceModsConfig.GetSourceModName("sourcemod_3"));
+        }
+
+        [TestMethod]
+        public void GetSourceModFolder()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+            });
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), string.Empty);
+
+            sourceModsConfig.LoadConfig();
+
+            Assert.AreEqual("sourcemod_1_folder", sourceModsConfig.GetSourceModFolder("sourcemod_1"));
+            Assert.AreEqual("sourcemod_2_folder", sourceModsConfig.GetSourceModFolder("sourcemod_2"));
+            Assert.AreEqual("sourcemod_3_folder", sourceModsConfig.GetSourceModFolder("sourcemod_3"));
+        }
+
+        [TestMethod]
+        public void GetSourceModDir()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+            });
+
+            const string SourceModsPath = "C:/Steam/steamapps/sourcemods";
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), SourceModsPath);
+
+            sourceModsConfig.LoadConfig();
+
+            Assert.AreEqual($"{SourceModsPath}/sourcemod_1_folder", sourceModsConfig.GetSourceModDir("sourcemod_1"));
+            Assert.AreEqual($"{SourceModsPath}/sourcemod_2_folder", sourceModsConfig.GetSourceModDir("sourcemod_2"));
+            Assert.AreEqual($"{SourceModsPath}/sourcemod_3_folder", sourceModsConfig.GetSourceModDir("sourcemod_3"));
+        }
+
+        [TestMethod]
+        public void GetSourceModDataDir()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+            });
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), string.Empty);
+
+            sourceModsConfig.LoadConfig();
+
+            Assert.AreEqual("./data/mods/sourcemod_1", sourceModsConfig.GetSourceModDataDir("sourcemod_1"));
+            Assert.AreEqual("./data/mods/sourcemod_2", sourceModsConfig.GetSourceModDataDir("sourcemod_2"));
+            Assert.AreEqual("./data/mods/sourcemod_3", sourceModsConfig.GetSourceModDataDir("sourcemod_3"));
+        }
+
+        [TestMethod]
+        public void IsSourceModInstalled()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+                { "C:/Steam/steamapps/sourcemods/sourcemod_1_folder", new MockDirectoryData() },
+                { "C:/Steam/steamapps/sourcemods/sourcemod_3_folder", new MockDirectoryData() },
+            });
+
+            const string SourceModsPath = "C:/Steam/steamapps/sourcemods";
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), SourceModsPath);
+
+            sourceModsConfig.LoadConfig();
+
+            Assert.IsTrue(sourceModsConfig.IsSourceModInstalled("sourcemod_1"));
+            Assert.IsFalse(sourceModsConfig.IsSourceModInstalled("sourcemod_2"));
+            Assert.IsTrue(sourceModsConfig.IsSourceModInstalled("sourcemod_3"));
+        }
+
+        [TestMethod]
+        public void GetInstalledSourceMods()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods.json")) },
+                { "C:/Steam/steamapps/sourcemods/sourcemod_1_folder", new MockDirectoryData() },
+                { "C:/Steam/steamapps/sourcemods/sourcemod_3_folder", new MockDirectoryData() },
+            });
+
+            const string SourceModsPath = "C:/Steam/steamapps/sourcemods";
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), SourceModsPath);
+
+            sourceModsConfig.LoadConfig();
+
+            CollectionAssert.AreEquivalent(new List<string> { "sourcemod_1", "sourcemod_3" }, sourceModsConfig.GetInstalledSourceMods());
+        }
+
+        [TestMethod]
+        public void SupportedSourceModsKeys_Sorted()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                { "C:/sourcemods_unsorted.json", new MockFileData(File.ReadAllBytes("../../../data/config/sourcemods_unsorted.json")) },
+            });
+
+            var sourceModsConfig = new SourceModsConfig(fileSystem, NullWriter, "C:/sourcemods_unsorted.json", new JSONConfigurationSerializer<JSONSourceModsConfig>(), string.Empty);
+
+            sourceModsConfig.LoadConfig();
+
+            CollectionAssert.AreEqual(new List<string> { "sourcemod_1", "sourcemod_2", "sourcemod_3" }, sourceModsConfig.SupportedSourceModsKeys);
         }
     }
 }
